8ee72aca350c4e6763af11b2431ef3753e010918
sonya7171 <144545207+sonya7171@users.noreply.github.com>
2023-10-06 20:57:09 +0300

    step4

---
+++ src/main/java/step4/StringCalculator.java
@@ -0,0 +1,50 @@
+package step4;
+
+import java.util.Scanner;
+public class StringCalculator {
+    public int add(String numbers) { //публічний нестатичний метод
+        int sum = 0;
+        if (!numbers.isEmpty()){
+            String delimiter = "[,\\n]";
+
+            if (numbers.endsWith("\n") || numbers.endsWith(",") ){
+                System.out.println("invalid value!");
+                return 0;
+            }
+
+            if (numbers.startsWith("//")) {
+                int newline_index = numbers.indexOf('\n');
+                if (newline_index != -1) {
+                    String delimiter_hand = numbers.substring(2, newline_index);  //встановлюємо роздільник
+                    delimiter = "[" + delimiter_hand + ",\\n]";   //тепер всі роздільники
+                    numbers = numbers.substring(newline_index + 1);   //обрізаємо частину рядка це роздільник
+                }
+            }
+
+            String[] numbers_broken = numbers.split(delimiter);   //розбиває рядок який містить числа на кожне число окремо, роздільник кома
+            for (int i = 0; i < numbers_broken.length; i++)
+            {
+                String number_str = numbers_broken[i].trim(); //trim  видаляє всі зайві пробіли на початку та в кінці рядка
+
+                try {
+                    int num = Integer.parseInt(number_str);  //перетворюємо рядок у ціле число
+                    sum += num;
+                } catch (NumberFormatException e) {
+                    System.out.println("invalid input!");
+                    return 0;
+                }
+            }
+        }
+        System.out.println("Sum:" + sum);
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner numb = new Scanner(System.in);  //створюємо об'єкт numb
+        System.out.println("Enter a numbers: ");
+        String number_line = numb.nextLine();  //зчитуємо рядок введений користувачем і вписуємо його в number
+        number_line = number_line.replace("\\n", "\n"); //замінено символи "\\n" на фактичний символ нового рядка "\n"
+        step4.StringCalculator obj_1 = new step4.StringCalculator(); // створюємо об’єкт obj_1
+        obj_1.add(number_line);
+    }
+}
+
